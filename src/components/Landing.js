566875cb8c3a8b37e26464c9a598f6e44d3d3e89
NatrezC <cnatrez@gmail.com>
2020-12-22 10:01:47 -0600

    styled the landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,30 @@ class Landing extends Component {
         return (
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className='landing-grid'>
-                    <Cell col={12}></Cell>
+                    <Cell col={12}>
+                        <img
+                            src="https://freesvg.org/img/1547510251.png"
+                            alt="avatar" //mean alternative (used just in case image wont show)
+                            className="avatarImg"
+                        />
+                        <div className="banner-text">
+                            <h1>Full Stack Software Engineer</h1>
+                            <hr /> 
+                            <p>HTML | CSS | JavaScript | NodeJS | PostgreSQL | MongoDB | React</p>
+                            <div className="social-links">
+                                {/* Github link */}
+                                <a href="https://github.com/NatrezC" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-github" aria-hidden="true"/>
+                                </a>
+                                
+                                {/* LinkedIn link */}
+                                <a href="https://www.linkedin.com/in/ndcrawford/" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-linkedin" aria-hidden="true" />
+                                </a>
+                            </div>
+                        </div>
+
+                    </Cell>
                 </Grid>
             </div>
         )
