15932ab955c76e5d3eec73e6d03d7418a0b66173
NatrezC <cnatrez@gmail.com>
2020-12-22 22:10:29 -0600

    added tabs section for my projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import {Tabs, Tab } from 'react-mdl'
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
     render() {
         return (
-            <div>
-                <h1>Project Page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>NodeJS</Tab>
+                    <Tab>PostgreSQL</Tab>
+                    <Tab>MongoDB</Tab>
+                </Tabs>
+                <section className="projects-grid">
+                    {this.toggleCategories()}
+                </section>
             </div>
         )
     }
