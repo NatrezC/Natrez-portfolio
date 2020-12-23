4325c6a21567d17957060cba8d5bd8e37716a702
NatrezC <cnatrez@gmail.com>
2020-12-22 22:20:35 -0600

    Created an if function to make sure im inside the right section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,35 @@ class Projects extends Component {
         super(props);
         this.state = { activeTab: 0 };
     }
+    
+    toggleCategories() {
+        if (this.state.activeTab === 0) {
+            return (
+                <div>
+                    <h1>this is React</h1>
+                </div>
+            )
+        } else if (this.state.activeTab === 1){
+            return (
+                <div>
+                    <h1>this is NodeJs</h1>
+                </div>
+            )
+        }else if (this.state.activeTab === 2) {
+            return (
+                <div>
+                    <h1>this is PostgreSQL</h1>
+                </div>
+            )
+        } else if (this.state.activeTab === 3) {
+            return (
+                <div>
+                    <h1>this is MongoDB</h1>
+                </div>
+            )
+        }
+    }
+
     render() {
         return (
             <div className="category-tabs">
